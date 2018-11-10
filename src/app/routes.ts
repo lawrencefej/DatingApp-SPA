7bc04cfffffe10f15ac4bb959e555e60bb1dcfc5
Lawrence <lawrencefej@gmail.com>
2018-11-10 11:35:00 -0500

    Configured member profile page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,18 +1,25 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailresolver } from './_resolvers/member-detail.resolver';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberListresolver } from './_resolvers/member-list.resolver';
 
 export const appRoutes: Routes = [
-    {path: 'e', component: HomeComponent},
+    {path: '', component: HomeComponent},
     {
         path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            {path: 'members', component: MemberListComponent},
+            {path: 'members', component: MemberListComponent,
+             resolve: {users: MemberListresolver}},
+            {path: 'member/:id', component: MemberDetailComponent,
+             resolve: {user: MemberDetailresolver}},
             {path: 'messages', component: MessagesComponent},
             {path: 'lists', component: ListsComponent},
         ]
