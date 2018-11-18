3e2a3812f4fecf3454b98920fce361138377bf87
Lawrence <lawrencefej@gmail.com>
2018-11-18 14:49:58 -0500

    Configured messagingfuntionality

---
+++ src/app/_models/message.ts
@@ -0,0 +1,14 @@
+export interface Message {
+    id: number;
+    senderId: number;
+    senderKnownAs: string;
+    senderPhotoUrl: string;
+    recipientId: number;
+    recipientKnownAs: string;
+    recipientPhotoUrl: string;
+    content: string;
+    isRRead: boolean;
+    dateRead: Date;
+    messageSent: Date;
+
+}
